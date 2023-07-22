7ee45f5763092b5751ef9f132ad61ddff5bdd770
Nicky <133660535+NicoleFormenti@users.noreply.github.com>
2023-07-22 16:33:03 +0100

    Add files via upload

---
+++ Final Project/pages/homepage/script.js
@@ -0,0 +1,100 @@
+const ul = document.querySelector('#dvd_list');
+
+//this displays the list of movies with a title, img, year, quantity available and price
+
+function appendChild(title, year, quantity, price, imageSource) {
+    const h1 = document.createElement('h1');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const img = document.createElement('img');
+    
+    h1.innerHTML = title;
+    h2.innerHTML = year;
+    h3.innerHTML = quantity;
+    h4.innerHTML = price;
+    img.src = imageSource;
+    h2.insertAdjacentText = h2.insertAdjacentText('afterbegin', 'Production Year:');
+    h3.insertAdjacentText = h3.insertAdjacentText('afterbegin', 'Available Stock:');
+    h4.insertAdjacentText = h4.insertAdjacentText('afterbegin', 'Price in £:');
+    
+    ul.appendChild(h1);
+    h1.appendChild(img);
+    h1.appendChild(h2);
+    h2.appendChild(h3);
+    h3.appendChild(h4);
+
+    //only 10 movies should appear on page 1 so we splice the array
+    console.log(movieList.slice(0, 10));
+    return movieList.slice(0, 10);
+
+}
+    
+for (
+    let i = 0;
+    i < 10;
+    i++
+){
+    appendChild(movieList[i].title, movieList[i].year, movieList[i].quantity, movieList[i].price, movieList[i].imageSource);
+}
+
+// sort array by year of production:
+function sortYear() {
+    const sortedList = movieList.sort( (a, b) => { //the sorted list is the "new" array
+        return a.year - b.year;
+    });
+    console.log(sortedList);
+    ul.innerHTML = '';
+    for(let i = 0; i < sortedList.length; i++){  
+        appendChild(sortedList[i].title, sortedList[i].year, sortedList[i].quantity, sortedList[i].price, sortedList[i].imageSource); // adding both label and category
+    }
+}
+
+//sort by price:
+function sortPrice() {
+    const newList = movieList.sort( (a, b) => { 
+        return a.price - b.price;
+        
+    });
+    console.log(newList);
+    ul.innerHTML = '';
+    for(let i = 0; i < newList.length; i++){  
+        appendChild(newList[i].title, newList[i].year, newList[i].quantity, newList[i].price, newList[i].imageSource); // adding both label and category
+    }
+}
+
+//search bar : 
+function searchMovie() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let search_title = document.getElementsByTagName('h1');
+      
+    for (i = 0; i < search_title.length; i++) { 
+        if (!search_title[i].innerHTML.toLowerCase().includes(input)) {
+            search_title[i].style.display="none";
+        }
+        else {
+            search_title[i].style.display="list-item";                 
+        }
+    }
+}
+
+//pagination:
+//la situazione normale deve essere su pagina 1, pagina 2 appare solo se si schiaccia sul due 
+function pageOne() {
+    const page = 1;
+    const step = 10;
+    const start = page * step - step;
+    const end = start + step;
+
+    console.log(movieList.slice(start, end));
+}
+
+function pageTwo() {
+    const page = 2;
+    const step = 10;
+    const start = page * step - step;
+    const end = start + step;
+
+    console.log(movieList.slice(start, end));
+}
